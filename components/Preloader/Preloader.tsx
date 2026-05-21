7576b963ef289e65a91947004d7517aeb12a55b2
s.p. <56647239+eternalvision@users.noreply.github.com>
2026-05-21 23:32:22 +0200

    logo

--- components/Preloader/Preloader.tsx
+++ components/Preloader/Preloader.tsx
@@ -28,7 +28,7 @@ export const Preloader = ({ onComplete }: Props) => {
             initial={{ opacity: 0, scale: 0.85 }}
             animate={{ opacity: 1, scale: 1, transition: { duration: 0.35, ease: 'easeOut' } }}
           >
-            <Logo size={200} animate />
+            <Logo size={200} preloader />
           </motion.div>
         </motion.div>
       )}
